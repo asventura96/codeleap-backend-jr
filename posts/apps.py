f5f0b374d01c0790d7680c3e93e45f317dbd1912
André Ventura <ti@asventura.com.br>
2025-10-07 02:18:46 -0300

    Adiciona documentação e comentários ao arquivo de configuração da aplicação posts

--- posts/apps.py
+++ posts/apps.py
@@ -1,6 +1,11 @@
+# posts/apps.py
+"""Configuration for the posts Django app."""
+
 from django.apps import AppConfig
 
 
 class PostsConfig(AppConfig):
+    """App configuration for the 'posts' app."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "posts"
